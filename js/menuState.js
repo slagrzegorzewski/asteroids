c391cbc51470d529baec43f9728c3a8460a8ed7f
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2016-12-28 23:02:38 +0100

    MenuState section initial, handle, update and draw, add script file to index.html

---
+++ js/menuState.js
@@ -0,0 +1,17 @@
+var MenuState = State.extend({
+    init: function(game){
+        this.game = game;
+    },
+
+    handleInput: function(input){
+
+    },
+
+    update: function(){
+
+    },
+
+    render: function(){
+
+    }
+});
